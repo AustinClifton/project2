443317c9b87b4c65c5c717664fbb76f29c260a27
AustinClifton <ajc4409@g.rit.edu>
2024-04-14 16:59:36 -0400

    commit v 1.0.5

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/project2/',
+  base: 'ajc4409/iste340/project2/',
   plugins: [react()],
 })
